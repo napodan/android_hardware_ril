3d9d18dbf99b2b1bc44b0f857261f73d23e664af
napodan <david.guenet@gmail.com>
2013-06-28 09:58:36 +0200

    Update to use system/core/include 4.2.2_r1.2

--- reference-cdma-sms/reference-cdma-sms.c
+++ reference-cdma-sms/reference-cdma-sms.c
@@ -23,13 +23,13 @@
 
 RIL_Errno wmsts_ril_cdma_decode_sms( RIL_CDMA_Encoded_SMS *  encoded_sms,
         RIL_CDMA_SMS_ClientBd * client_bd) {
-    LOGE("ril_cdma_decode_sms function not implemented\n");
+    ALOGE("ril_cdma_decode_sms function not implemented\n");
     return RIL_E_GENERIC_FAILURE;
   }
 
 RIL_Errno wmsts_ril_cdma_encode_sms(RIL_CDMA_SMS_ClientBd * client_bd,
         RIL_CDMA_Encoded_SMS *  encoded_sms) {
 
-    LOGE("ril_cdma_encode_sms function not implemented\n");
+    ALOGE("ril_cdma_encode_sms function not implemented\n");
     return RIL_E_GENERIC_FAILURE;
   }
